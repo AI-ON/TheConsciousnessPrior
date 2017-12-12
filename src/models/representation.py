19e4a3db4477e3e710046a197c290858160f3a5f
William Fedus <liam.fedus@gmail.com>
2017-12-12 11:33:39 -0500

    Adding in a DCGAN-inspired image encoder and a train_model file

--- src/models/representation.py
+++ src/models/representation.py
@@ -1,12 +1,55 @@
 import tensorflow as tf
 
-def encoder(image, FLAGS):
+flags = tf.app.flags
+FLAGS = flags.FLAGS
+
+def encoder(image, is_train=True, reuse=False):
     """Encoder from raw pixels to an embedding."""
+    # Adopting a simple DCGAN-inspired image encoder.
+    num_filters = 32
+    
+    with tf.variable_scope("encoder", reuse=reuse):
+        conv_0 = tf.layers.conv2d(image, 
+                                 filters=num_filters, 
+                                 kernel_size=(5,5), 
+                                 strides=(2,2),
+                                 activation=None)
+        lrelu_0 = tf.nn.leaky_relu(conv_0, 0.2)
+        conv_1 = tf.layers.conv2d(lrelu_0, 
+                                 filters=num_filters*2, 
+                                 kernel_size=(5,5), 
+                                 strides=(2,2),
+                                 activation=None)
+        batch_norm_1 = tf.layers.batch_normalization(conv_1, training=is_train)
+        lrelu_1 = tf.nn.leaky_relu(batch_norm_1, 0.2)
+        conv_2 = tf.layers.conv2d(lrelu_1, 
+                                 filters=num_filters*4, 
+                                 kernel_size=(5,5), 
+                                 strides=(2,2),
+                                 activation=None)
+        batch_norm_2 = tf.layers.batch_normalization(conv_2, training=is_train)
+        lrelu_2 = tf.nn.leaky_relu(batch_norm_2, 0.2)
+        conv_3 = tf.layers.conv2d(conv_2, 
+                                 filters=num_filters*8, 
+                                 kernel_size=(5,5), 
+                                 strides=(2,2),
+                                 activation=None)
+        batch_norm_3 = tf.layers.batch_normalization(conv_3, training=is_train)
+        lrelu_3 = tf.nn.leaky_relu(batch_norm_3, 0.2)
+
+        # Flatten convolutional feature maps.
+        flattened = tf.layers.flatten(lrelu_3)
+        embedding = tf.layers.dense(flattened,
+                                    units=FLAGS.representation_dim)
+        return embedding
+
+
+def representation_recurrence(image_encoding):
+    """Recurrent component of representation."""
     pass
 
-def representation(image_encoding, FLAGS):
-    """Representation RNN (R)."""
 
-    lstm = tf.contrib.rnn.LSTM(FLAGS.representation_dim)
-
+def representation(FLAGS):
+    """Representation (R)"""
+    pass    
 
--- src/train_model.py
+++ src/train_model.py
@@ -1,19 +1,51 @@
 import tensorflow as tf
 
+from models.representation import *
+
 flags = tf.app.flags
-FLAGS = flags.FLAGS
 
 flags.DEFINE_float('learning_rate', 0.0001, 'Initial learning rate.')
-flags.DEFINE_integer('max_steps', 2000, 'Number of steps to run trainer.')
+flags.DEFINE_integer('training_steps', 2000, 'Number of steps to train model.')
 flags.DEFINE_integer('time_steps', 5, 'Number of time steps to unroll model')
 flags.DEFINE_integer('representation_dim', 128, 'Number of units in hidden units in RNN layer.')
+flags.DEFINE_integer('image_dim', 128, 'Dimension of the image frames.')
 flags.DEFINE_integer('batch_size', 128, 'Batch size.  '
                          'Must divide evenly into the dataset sizes.')
 
+FLAGS = flags.FLAGS
+
+def create_model():
+    """Create the model."""
+    # Dummy image for testing shape.  Our model will operate over a video
+    # sequence.
+    image = tf.random_normal(shape=[FLAGS.batch_size, 
+                                    FLAGS.representation_dim,
+                                    FLAGS.representation_dim, 3])
+    embedding = encoder(image)
+    print(embedding.get_shape())
+
+    model = embedding
+    return model
+
+
+def create_loss():
+    pass
+
+
+def create_optimizer():
+    pass
+
+
 def create_graph():
     """Create computational graph."""
+    output = create_model()
+    
+
+def train_model():
+    """Train the model."""
     pass
 
 
 if __name__=="__main__":
-    model = create_graph()
+    create_graph()
+
