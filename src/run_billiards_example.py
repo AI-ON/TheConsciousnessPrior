d85f568920d952e67a0b6b74828140f058134f4b
William Fedus <liam.fedus@gmail.com>
2017-12-15 17:37:23 -0500

    Saving the billiards output as a gif rather than as 128 separate images

--- src/run_billiards_example.py
+++ src/run_billiards_example.py
@@ -1,19 +1,26 @@
 """Billiards Example."""
 from __future__ import print_function
-from builtins import range
 
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 from environments.billiards import Billiards
 
-env = Billiards()
+resolution = 128
+time_steps = 25
 
-
-env.make_frames(200)
+env = Billiards(T=time_steps)
+env.make_frames(resolution)
 
 frames = env.frames
+num_frames = frames.shape[0]
 
-plt.figure()
-for t in range(frames.shape[0]):
+fig = plt.figure()
+
+def update(t):
     plt.imshow(frames[t], cmap="gray")
-    plt.show()
+
+if __name__=="__main__":
+    # FuncAnimation will call the 'update' function for each frame
+    anim = FuncAnimation(fig, update, frames=time_steps, interval=200)
+    anim.save('billiards.gif',writer='imagemagick');
